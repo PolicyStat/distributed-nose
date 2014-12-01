f5075cbe97d90635b690df88e9f515245ad89d97
winhamwr <winhamwr@gmail.com>
2014-12-01 16:58:26 -0500

    It's time for 1.0.0 bump!

--- distributed_nose/__init__.py
+++ distributed_nose/__init__.py
@@ -1,5 +1,5 @@
 """Distribute your nose tests across multiple machines, hassle-free"""
-VERSION = (0, 1, 2, '')
+VERSION = (1, 0, 0, '')
 __version__ = '.'.join(map(str, VERSION[0:3])) + ''.join(VERSION[3:])
 __author__ = 'Wes Winham'
 __contact__ = 'winhamwr@gmail.com'
